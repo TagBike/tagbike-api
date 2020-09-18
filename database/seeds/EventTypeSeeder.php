e5cf57dcd1166d6033b8d0d0909931cd6991bfbb
gahfaustino <gahfaustino@gmail.com>
2020-09-18 18:27:37 -0300

    update eventtypeseeder

--- database/seeds/EventTypeSeeder.php
+++ database/seeds/EventTypeSeeder.php
@@ -13,8 +13,61 @@ class EventTypeSeeder extends Seeder
     public function run()
     {        
         if (!EventType::count()) {
-            $arrEventNames = ['Administrador', 'resetou senha de cliente', 'null', 'null', 'null', 'null'];
-            $arrEventKeys = ['event', 'action', 'user', 'customer', 'account', 'resetpassword'];
+            $arrEventNames = [
+                "event.action.administration.customer.account.created",
+                "event.action.administration.customer.blockedaccount",
+                "event.action.administration.customer.blockedaccount.reason",
+                "event.action.customer.account.new",
+                "event.action.customer.account.update",
+                "event.action.customer.bike.add",
+                "event.action.customer.bike.archive",
+                "event.action.customer.bike.update",
+                "event.action.customer.bike.search",
+                "event.action.customer.bike.search.ownbike",
+                "event.action.customer.bike.changeproperty",
+                "event.action.sos.customer.accident",
+                "event.action.sos.customer.wasstolen",
+                "event.action.sos.bike.wasstolen",
+                "event.action.user.customer.account.new",
+                "event.action.user.customer.account.update",
+                "event.action.user.customer.account.resetpassword",
+                "event.action.user.account.new",
+                "event.action.user.account.update",
+                "event.action.user.account.resetpassword",
+                "event.system.administration.customer.blockedaccount",
+                "event.system.administration.customer.blockedaccount.reason",
+                "event.log.customer.web.access",
+                "event.log.customer.web.app.android",
+                "event.log.customer.web.app.ios",               
+            ];
+            $arrEventKeys = [
+                "event.action.administration.customer.account.created",
+                "event.action.administration.customer.blockedaccount",
+                "event.action.administration.customer.blockedaccount.reason",
+                "event.action.customer.account.new",
+                "event.action.customer.account.update",
+                "event.action.customer.bike.add",
+                "event.action.customer.bike.archive",
+                "event.action.customer.bike.update",
+                "event.action.customer.bike.search",
+                "event.action.customer.bike.search.ownbike",
+                "event.action.customer.bike.changeproperty",
+                "event.action.sos.customer.accident",
+                "event.action.sos.customer.wasstolen",
+                "event.action.sos.bike.wasstolen",
+                "event.action.user.customer.account.new",
+                "event.action.user.customer.account.update",
+                "event.action.user.customer.account.resetpassword",
+                "event.action.user.account.new",
+                "event.action.user.account.update",
+                "event.action.user.account.resetpassword",
+                "event.system.administration.customer.blockedaccount",
+                "event.system.administration.customer.blockedaccount.reason",
+                "event.log.customer.web.access",
+                "event.log.customer.web.app.android",
+                "event.log.customer.web.app.ios",
+                   
+            ];
             $arrEvent = array_map(null, $arrEventNames, $arrEventKeys);
 
             foreach ($arrEvent as $event) {
