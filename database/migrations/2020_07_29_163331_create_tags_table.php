792783e36ed62127e9c9e2b5daad8522ff690470
gahfaustino <gahfaustino@gmail.com>
2020-09-23 04:29:24 -0300

    change schema to tags table

--- database/migrations/2020_07_29_163331_create_tags_table.php
+++ database/migrations/2020_07_29_163331_create_tags_table.php
@@ -15,10 +15,8 @@ class CreateTagsTable extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->integer('id_bike');
-            $table->text('qr_code');
-            $table->string('qr_img');
+            $table->text('hash');
             $table->timestamps();
             $table->softDeletes();
         });
